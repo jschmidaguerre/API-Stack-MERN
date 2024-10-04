4dff88051bf0bcdd2041e5398938399bc1e16d54
jschmidaguerre <juanseaguerre@gmail.com>
2024-10-04 13:58:01 -0300

    app 100% functional deployed on cloud atlas

--- backend/src/models/models.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { userSchema } = require('./usuario.model').default;
-
-const usuario = mongoose.model('usuario', userSchema);
-
-
-module.exports = {
-    usuario
-};
